0981a9510edaa72410a38f61ca00b150294f1f0e
CJ Kihlbom <cj@elabs.se>
2011-10-27 10:41:41 +0200

    Refactored matchers in step definition spec

--- spec/step_definition_spec.rb
+++ spec/step_definition_spec.rb
@@ -2,20 +2,20 @@ describe Turnip::StepDefinition do
   describe "#match" do
     it "matches alternative words" do
       step = Turnip::StepDefinition.new("there is/are monsters") {}
-      step.match("there are monsters").should_not be_nil
-      step.match("there is monsters").should_not be_nil
+      step.should match("there are monsters")
+      step.should match("there is monsters")
     end
     
     it "matches several alternative words" do
       step = Turnip::StepDefinition.new("monsters are cool/nice/scary") {}
-      step.match("monsters are cool").should_not be_nil
-      step.match("monsters are nice").should_not be_nil
-      step.match("monsters are scary").should_not be_nil
+      step.should match("monsters are cool")
+      step.should match("monsters are nice")
+      step.should match("monsters are scary")
     end
     
     it "matches optional parts of words" do
       step = Turnip::StepDefinition.new("there is/are X monster(s)") {}
-      step.match("there is X monster").should_not be_nil
+      step.should match("there is X monster")
     end
   end
 end
