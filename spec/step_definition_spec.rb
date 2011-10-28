dce8a22e1321453a2803ff5c5e8d7aae556082a2
Your Name <name@elabs.se>
2011-10-28 16:43:52 +0200

    Test negative case as well

--- spec/step_definition_spec.rb
+++ spec/step_definition_spec.rb
@@ -4,24 +4,28 @@ describe Turnip::StepDefinition do
       step = Turnip::StepDefinition.new("there is/are monsters") {}
       step.should match("there are monsters")
       step.should match("there is monsters")
+      step.should_not match("there be monsters")
     end
-    
+
     it "matches several alternative words" do
       step = Turnip::StepDefinition.new("monsters are cool/nice/scary") {}
       step.should match("monsters are cool")
       step.should match("monsters are nice")
       step.should match("monsters are scary")
+      step.should_not match("monsters are sexy")
     end
-    
+
     it "matches optional parts of words" do
       step = Turnip::StepDefinition.new("there is/are X monster(s)") {}
       step.should match("there is X monster")
+      step.should_not match("there is X monsterQ")
     end
 
     it "matches optional words" do
       step = Turnip::StepDefinition.new("there is a (scary) monster") {}
       step.should match("there is a monster")
       step.should match("there is a scary monster")
+      step.should_not match("there is a terrifying monster")
 
       step = Turnip::StepDefinition.new("there is a monster (that is scary)") {}
       step.should match("there is a monster that is scary")
@@ -33,3 +37,4 @@ describe Turnip::StepDefinition do
     end
   end
 end
+
