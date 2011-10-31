b6217d72aafe43cbb78d65713be150f1f6416fba
Jonas Nicklas <jonas.nicklas@gmail.com>
2011-10-31 06:32:22 +0100

    Simplify integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,44 +1,16 @@
 require 'spec_helper'
 
 describe 'The CLI', :type => :integration do
-  describe 'rspec -fs examples/simple_feature.feature' do
-    it "shows the correct description" do
-      result.should include('A simple feature')
-      result.should include('is a simple feature')
-    end
-
-    it "passes" do
-      result.should include('1 example, 0 failures')
-    end
+  before do
+    @result = %x(rspec -fs examples/*.feature)
   end
 
-  describe 'rspec -fs examples/ambiguous.feature' do
-    it "fails" do
-      result.should include('1 example, 1 failure')
-    end
+  it "shows the correct description" do
+    @result.should include('A simple feature')
+    @result.should include('is a simple feature')
   end
 
-  describe 'rspec -fs examples/backgrounds.feature' do
-    it "passes" do
-      result.should include('1 example, 0 failures')
-    end
-  end
-
-  describe 'rspec -fs examples/interpolation.feature' do
-    it "passes" do
-      result.should include('3 examples, 0 failures')
-    end
-  end
-
-  describe 'rspec -fs examples/pending.feature' do
-    it "is marked as pending" do
-      result.should include('1 example, 0 failures, 1 pending')
-    end
-  end
-
-  describe 'rspec -fs examples/tags.feature' do
-    it "passes" do
-      result.should include('2 examples, 0 failures')
-    end
+  it "prints out failures and successes" do
+    @result.should include('9 examples, 1 failure, 1 pending')
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,4 @@
-module IntegrationHelpers
-  def command
-    example.metadata[:example_group][:description_args].first
-  end
-
-  def run_command
-    %x(#{command})
-  end
-
-  def result
-    @result ||= run_command
-  end
-end
-
 RSpec.configure do |config|
-  config.include IntegrationHelpers, :type => :integration
   config.before(:each, :turnip => true) do
     require File.expand_path('../examples/steps', File.dirname(__FILE__))
   end
