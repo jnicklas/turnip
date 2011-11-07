41de172123007e46836e0c1dff23519bed3138c6
Les Hill <leshill@gmail.com>
2011-11-06 22:06:48 -0800

    Minimize global DSL to steps_for

--- lib/turnip/dsl.rb
+++ lib/turnip/dsl.rb
@@ -1,24 +1,7 @@
 module Turnip
   module DSL
-    class << self
-      attr_accessor :current_taggings
-    end
-
-    def step(description, options={}, &block)
-      if Turnip::DSL.current_taggings
-        options[:for] = [options[:for], *Turnip::DSL.current_taggings].compact.flatten
-      end
-      Turnip::StepDefinition.add(description, options, &block)
-    end
-
-    def steps_for(*taggings)
-      Turnip::DSL.current_taggings = [taggings, *Turnip::DSL.current_taggings].compact.flatten
-      yield
-      Turnip::DSL.current_taggings = nil
-    end
-
-    def placeholder(name, &block)
-      Turnip::Placeholder.add(name, &block)
+    def steps_for(*taggings, &block)
+      Turnip::StepModule.steps_for(*taggings, &block)
     end
   end
 end
--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -6,49 +6,10 @@ describe Turnip::DSL do
     @context.extend(Turnip::DSL)
   end
 
-  describe '#step' do
-    it 'adds a step to the list of step definitions' do
-      Turnip::StepDefinition.should_receive(:add).with('this is a :thing test', {})
-      @context.step 'this is a :thing test'
-    end
-
-    it 'sends through options' do
-      Turnip::StepDefinition.should_receive(:add).with('foo', {:for => [:monkey]})
-      @context.step 'foo', :for => [:monkey]
-    end
-  end
-
   describe '#steps_for' do
-    it 'executes the given block and adds steps with for set' do
-      Turnip::StepDefinition.should_receive(:add).with('foo', {:for => [:gorilla]})
-      @context.steps_for :gorilla do
-        @context.step 'foo'
-      end
-    end
-
-    it 'combines step for option and block options' do
-      Turnip::StepDefinition.should_receive(:add).with('foo', {:for => [:a, :b, :gorilla]})
-      @context.steps_for :gorilla do
-        @context.step 'foo', :for => [:a, :b]
-      end
-    end
-
-    it 'can be nested' do
-      Turnip::StepDefinition.should_receive(:add).with('foo', {:for => [:c, :b, :a]})
-      @context.steps_for :a do
-        @context.steps_for :b do
-          @context.step 'foo', :for => :c
-        end
-      end
-    end
-  end
-
-  describe '#placeholder' do
-    it 'adds a placeholder to the list of placeholders' do
-      @context.placeholder :quox do
-        match(/foo/) { 'bar' }
-      end
-      Turnip::Placeholder.apply(:quox, 'foo').should == 'bar'
+    it 'delegates to StepModule' do
+      Turnip::StepModule.should_receive(:steps_for).with(:example)
+      @context.steps_for(:example) {}
     end
   end
 end
