b01b755fb9f3535d901af37f2a26d43ee63a3be2
Jonas Nicklas <jonas.nicklas@gmail.com>
2012-05-10 22:28:52 +0200

    unit tests for extracting arguments from steps

--- spec/step_definition_spec.rb
+++ spec/step_definition_spec.rb
@@ -17,6 +17,25 @@ describe Turnip::StepDefinition do
       step.should_not match("there are no monsters")
     end
 
+    it "extracts arguments from matched steps" do
+      step = Turnip::StepDefinition.new("a :age year old monster called :name") {}
+      match = step.match("a 3 year old monster called John")
+      match.params.should eq(["3", "John"])
+    end
+
+    it "can reuse the same placeholder multiple times" do
+      step = Turnip::StepDefinition.new("a the monsters :name and :name") {}
+      match = step.match("a the monsters John and Trudy")
+      match.params.should eq(["John", "Trudy"])
+    end
+
+    it "can reuse the same custom placeholder multiple times" do
+      Turnip::Placeholder.stub(:resolve).with(:count).and_return(/\d+/)
+      step = Turnip::StepDefinition.new(":count monsters and :count knights") {}
+      match = step.match("3 monsters and 2 knights")
+      match.params.should eq(["3", "2"])
+    end
+
     it "matches quoted placeholders" do
       step = Turnip::StepDefinition.new("there is a monster named :name") {}
       step.should match("there is a monster named 'Scary'")
