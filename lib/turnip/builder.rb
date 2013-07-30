cb5294e880a847195f36c1e107fa6b599936f7d8
maf <maf@runtastic.com>
2013-07-30 13:55:33 +0200

    Fix according to the given step.rows class use to_a or (&:value) to retrieve its values

--- lib/turnip/builder.rb
+++ lib/turnip/builder.rb
@@ -153,7 +153,11 @@ module Turnip
       if step.doc_string
         extra_args.push step.doc_string.value
       elsif step.rows
-        extra_args.push Turnip::Table.new(step.rows.map { |row| row.cells(&:value) })
+        if step.rows.is_a? Java::JavaUtil::ArrayList
+          extra_args.push Turnip::Table.new(step.rows.map { |row| row.cells.to_a })
+        else
+          extra_args.push Turnip::Table.new(step.rows.map { |row| row.cells(&:value) })
+        end
       end
       @current_step_context.steps << Step.new(step.name, extra_args, step.line)
     end
