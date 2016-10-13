9eb043115a44ee99f3fc2bc48ce57fd88fa2ba0d
Wataru MIYAGUNI <gonngo@gmail.com>
2016-10-13 23:09:47 +0900

    A feature node is created per a gherkin document.

--- lib/turnip/builder.rb
+++ lib/turnip/builder.rb
@@ -3,27 +3,12 @@ require 'turnip/node/feature'
 
 module Turnip
   class Builder
+    def self.build(feature_file)
+      parser = Gherkin::Parser.new
+      result = parser.parse(File.read(feature_file))
 
-    attr_reader :features
-
-    class << self
-      def build(feature_file)
-        Turnip::Builder.new.tap do |builder|
-          parser = Gherkin::Parser.new
-          result = parser.parse(File.read(feature_file))
-          builder.build(result)
-        end
-      end
-    end
-
-    def initialize
-      @features = []
-    end
-
-    def build(attributes)
-      return unless attributes[:feature]
-
-      @features << Node::Feature.new(attributes[:feature])
+      return nil unless result[:feature]
+      Node::Feature.new(result[:feature])
     end
   end
 end
