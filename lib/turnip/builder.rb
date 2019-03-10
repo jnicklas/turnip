2e27c58702648a35c7e24148a7b7efbac6d1c3db
Ritchie Young <ritchiey@gmail.com>
2019-03-10 14:11:43 +0800

    remove use of Ruby 2.3 dependent safe-navigation operator

--- lib/turnip/builder.rb
+++ lib/turnip/builder.rb
@@ -5,10 +5,11 @@ module Turnip
   class Builder
     def self.build(feature_file)
       messages = Gherkin::Gherkin.from_paths([feature_file], include_source: false, include_pickles: false)
-      result = messages.first&.gherkinDocument&.to_hash
-
-      return nil if result.nil? || result[:feature].nil?
-      Node::Feature.new(result[:feature])
+      message = messages.first or return nil
+      gherkinDocument = message.gherkinDocument or return nil
+      result = gherkinDocument.to_hash or return nil
+      feature = result[:feature] or return nil
+      Node::Feature.new(feature)
     end
   end
 end
