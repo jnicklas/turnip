6ae3a972b83dd16aff1211056ba0c95f077bcb85
Peter Toth <tothpeter08@gmail.com>
2019-12-17 13:46:34 +0100

    Update Gherkin version to 9.0

--- lib/turnip/builder.rb
+++ lib/turnip/builder.rb
@@ -10,7 +10,7 @@ module Turnip
         include_gherkin_document: true,
         include_pickles: false
       )
-      result = messages.first&.gherkinDocument&.to_hash
+      result = messages.first&.gherkin_document&.to_hash
 
       return nil if result.nil? || result[:feature].nil?
       Node::Feature.new(result[:feature])
